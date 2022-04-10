c7b466dc624d105bec16fce4b805cf1eaec34877
sahil78272026 <sahil.garg78272026@gmail.com>
2022-04-10 20:22:48 +0530

    Add files via upload

---
+++ 04_tuples.py
@@ -0,0 +1,13 @@
+# creating a tuple using ()
+t = (1,3,5,7,4)
+print(t)
+# tuples are immutables , cannot be changed , major difference between list and tuple
+
+# t[0]= 4 # throws an error
+
+t1 = () # empty tuple
+print(t1)
+
+# t2 = (1) wrong way to declare tuple with single element
+t2 = (1,) # right way to declare tuple with single element
+print(t2)
